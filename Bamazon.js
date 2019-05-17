3ad57351040f592c71add64305c0ca94e272baa6
Brianda Barrios <briandabarrios@Briandas-MacBook-Pro.local>
2019-05-17 10:14:57 -0700

    update

--- Bamazon.js
+++ Bamazon.js
@@ -1,7 +1,7 @@
-
+//dependecies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+// connection to SQL data base
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,32 +9,34 @@ var connection = mysql.createConnection({
   password: "spring2019",
   database: "BamazonDB"
 });
-
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("Testing connection" + connection.threadId + "\n");
-  createProduct();
+  console.log("MySQL connection" + connection.threadId);
+  table();
 });
-
-function createProduct() {
-  console.log("Inserting a new product...\n");
+//showing table "items"
+function table() {
   var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      item_id: "001",
-      product_name: "shampoo for women",
-      department_name: "beauty products",
-      price: 3.0,
-      stock_quantity: 50
-    },
+    "SELECT * FROM items",
     function(err, res) {
-      console.log(res.affectedRows + " product inserted!\n");
-      // Call updateProduct AFTER the INSERT completes
+      console.table(res);
     }
   );
+};
+var express = require("express");
+var path = require("path");
 
+var app = express();
+var PORT = 3000;
 
-  // logs the actual query being run
-  console.log(query.sql);
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
